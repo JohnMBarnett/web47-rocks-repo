477a98c7a6f21d6dc9cb43cc6fc67c77872457ea
john barnett <jomibaofficial@gmail.com>
2021-10-28 11:56:22 -0500

    work in progress

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-const express = requier('express');
+require('dotenv').config();
+
+const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-    res.send('<h1>web 47 rocks!</h1')
+    res.send('<h1>web 47 rocks!</h1');
+});
+
+app.get('/port', (req, res) => {
+    res.send(`<h1>${PORT}</h1>`);
 });
 
 app.listen(PORT, () => {
