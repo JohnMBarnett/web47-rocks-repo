6a7e9ff65c0349f7d7b3c048500fef43a09d4bc1
john barnett <jomibaofficial@gmail.com>
2021-10-28 11:48:48 -0500

    test

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = requier('express');
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('<h1>web 47 rocks!</h1')
+});
+
+app.listen(PORT, () => {
+    console.log(`listening on ${PORT}`);
+})
+
+
+
+
+
+
+
+
+
+function cohortRocks() {
+    console.log(`web ${process.argv[2]} rocks!`);
+}
+
+function cohortRocksEnv() {
+    console.log(`home folder is ${process.env.HOME}`);
+    console.log(`my shell is ${process.env.SHELL}`);
+    console.log(`my environment is ${process.env.FOO}`);
+    console.log('my super secret comes from',  process.env.SUPER_SECRET);
+}
+
+cohortRocksEnv()
